bb81568a327257b8ad8e13a88655f801215713de
Nkoking <nkogitcontact@gmail.com>
2017-11-30 19:57:38 +0200

    Created tree generator

---
+++ Tree-generator.py
@@ -0,0 +1,25 @@
+import random
+#random characters for decorations
+chars = ["0","o","@"," *"]
+
+#measurements
+width = 40
+standwidth = 40
+standbottom = str(chr(9620) * 4)
+#printtaa tähti
+print(" " * round(width/2 + .5) + "★" + " " * round(width/2 + .5))
+#For the length of the tree print lines
+for layer in range(1,20):
+    #print whitespace and a pineneedle
+    print(" " * round(width/2 + .5 - 1) + "<", end="")
+    #for loop to print random characters
+    for i in range(layer):
+        print(random.choice(chars), end="")
+        #print pineneedles and whitespace
+    print(">" + " " * round(width/2 + .5 - 1))
+    #delete whitespace
+    width -= 1
+#printtaa jalusta
+print(" " * round(standwidth/2 - 1) + "|" + "  " + "|" + " " * round(standwidth/2 - 3.5))
+print(" " * round(standwidth/2 - 2) + standbottom + " " * round(standwidth/2))
+
