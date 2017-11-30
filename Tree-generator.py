505c419da93b2af91e94de0b4836cef916f23697
Nkoking <nkogitcontact@gmail.com>
2017-11-30 20:51:40 +0200

    Fixed comments

--- Tree-generator.py
+++ Tree-generator.py
@@ -6,9 +6,9 @@ chars = ["0","o","@"," *"]
 width = 40
 standwidth = 40
 standbottom = str(chr(9620) * 4)
-#printtaa tähti
+#print the star
 print(" " * round(width/2 + .5) + "★" + " " * round(width/2 + .5))
-#For the length of the tree print lines
+#For the length of the tree print the tree
 for layer in range(1,20):
     #print whitespace and a pineneedle
     print(" " * round(width/2 + .5 - 1) + "<", end="")
@@ -19,7 +19,7 @@ for layer in range(1,20):
     print(">" + " " * round(width/2 + .5 - 1))
     #delete whitespace
     width -= 1
-#printtaa jalusta
+#print the stand
 print(" " * round(standwidth/2 - 1) + "|" + "  " + "|" + " " * round(standwidth/2 - 3.5))
 print(" " * round(standwidth/2 - 2) + standbottom + " " * round(standwidth/2))
 
