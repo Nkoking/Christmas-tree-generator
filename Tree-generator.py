07c7925b0ce7c9a365c5b7afbe016e02cedf9d1a
Nkoking <nkogitcontact@gmail.com>
2017-12-01 17:04:31 +0200

    Fixed tree layer lenghts

--- Tree-generator.py
+++ Tree-generator.py
@@ -1,25 +1,26 @@
 import random
+import math
 #random characters for decorations
-chars = ["0","o","@"," *"]
+chars = ["#", "@","*"]
 
 #measurements
 width = 40
 standwidth = 40
-standbottom = str(chr(9620) * 4)
+standbottom = str(chr(9620) * 5)
 #print the star
-print(" " * round(width/2 + .5) + "★" + " " * round(width/2 + .5))
+print(" " * math.ceil(width/2 + 2) + "★" + " " * round(width/2 + .5))
 #For the length of the tree print the tree
 for layer in range(1,20):
     #print whitespace and a pineneedle
-    print(" " * round(width/2 + .5 - 1) + "<", end="")
+    print(" " * math.ceil(width/2) + "<", end="")
     #for loop to print random characters
-    for i in range(layer):
+    for i in range(layer * 2 + 1):
         print(random.choice(chars), end="")
-        #print pineneedles and whitespace
-    print(">" + " " * round(width/2 + .5 - 1))
+    #print pineneedles
+    print(">")
     #delete whitespace
-    width -= 1
+    width -= 2
 #print the stand
-print(" " * round(standwidth/2 - 1) + "|" + "  " + "|" + " " * round(standwidth/2 - 3.5))
-print(" " * round(standwidth/2 - 2) + standbottom + " " * round(standwidth/2))
+print(" " * math.ceil(standwidth/2) + "|" + "  " + "|")
+print(" " * math.ceil(standwidth/2 - 1) + standbottom)
 
